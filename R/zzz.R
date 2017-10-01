81825c275d431ddf4cc6fa58ebb32454bf9d0746
neyhartj <neyhartje@gmail.com>
2017-10-01 12:25:39 -0500

    Remove LSD functions for testing

--- R/zzz.R
+++ R/zzz.R
@@ -1,5 +1,5 @@
 # This script is from the tidyverse package
-load <- c("ggplot2", "tidyr", "readr", "dplyr", "lme4", "agridat")
+load <- c("dplyr", "readr", "lme4", "agridat")
 
 # Function to run when the package is attached
 .onAttach <- function(...) {
